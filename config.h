a1d5f503f50a26f4a87d423602a43fdd71b28b0d
Richard Chen <i@iochen.com>
2022-11-22 23:07:19 +0800

    add config module

---
+++ config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+typedef struct {
+    // filename can NOT be NULL or empty
+    char *filename;
+    // as a bool value
+    int help;
+    // NULL for argument not supplied
+    char *cache;
+    // as a bool value
+    int no_audio;
+    // fps <= 0 for srgument not supplied
+    int fps;
+} config;
+
+config parse_config(int argc, char *argv[]);
+
+#endif
